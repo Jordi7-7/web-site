4d82c42edf7e2c24ac69b3fdd4c0b4feed120564
Jordi <jordi.cahuas20@gmail.com>
2024-09-18 11:01:47 -0500

    add sections Nav, Hero, Carrousel without functionality, Sponsors card in progress

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			keyframes: {
+				float: {
+				  '0%, 100%': { transform: 'translateY(0)' },
+				  '50%': { transform: 'translateY(-10px)' },
+				},
+			  },
+			  animation: {
+				float: 'float 3s ease-in-out infinite',
+			  },
+			clipPath: {
+				'custom-clip': 'polygon(100% 0, 0 0, 0 100%, 69% 100%, 69% 81%, 85% 81%, 85% 67%, 100% 67%)',
+			}
+			  ,
+			backgroundImage: {
+				'hero-image':"url('/hero-image.jpg')"
+			}
+		},
+	},
+	plugins: [
+		require('tailwind-clip-path'),
+	],
+}
